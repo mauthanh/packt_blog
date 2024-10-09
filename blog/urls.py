726e239428be7fe133568ec33e1e6961cf3fe6f2
mauthanh <thanh.truongmau@gmail.com>
2024-10-09 23:36:48 +0700

    create RSS feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-from .feeds import LatestPostFeed
-
 app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -15,5 +13,4 @@ urlpatterns = [
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-    path('feed/', LatestPostFeed(), name='post_feed'),
 ]
